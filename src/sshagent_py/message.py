5c97fbd41debdaf4bb8695de8a6711989829762d
Ganesh Tiwari <ganesht049@gmail.com>
2026-09-19 22:32:59 +0530

    fix: trying to figure out decoding and encoding
    Signed-off-by: Ganesh Tiwari <ganesht049@gmail.com>

--- src/sshagent_py/message.py
+++ src/sshagent_py/message.py
@@ -4,10 +4,10 @@ from .types import SSH_Messages
 def decode_message_bytes(data: bytes) -> SSH_Messages:
   message_length = data[:4]
   try:
-    if int.from_bytes(message_length) > 0:
+    int_message_length = int.from_bytes(message_length)
+    if int_message_length > 0:
       message_type = data[4]
       contents = data[5:]
-
       if message_type == SSH_Messages.SSH_AGENTC_REQUEST_IDENTITIES.value.to_bytes():
         print("listing identities")
         return SSH_Messages.SSH_AGENTC_REQUEST_IDENTITIES
